686c3832c83396f0083947dfdbe6371dbac96026
Raphael Azeredo <rgazeredo@gmail.com>
2014-09-20 01:18:25 -0300

    Access web services

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,11 @@
 var checkdomain = angular.module('checkdomain.controllers', [])
 
-checkdomain.controller('HomeCtrl', function($scope, $ionicPopup) {
+checkdomain.controller('HomeCtrl', function($scope, $ionicPopup, $http) {
     $scope.dataDomain = {
         domain: "",
         extension: "com.br"
     };
+
     $scope.showResult = false;
 
     $scope.searchDomain = function() {
@@ -14,10 +15,23 @@ checkdomain.controller('HomeCtrl', function($scope, $ionicPopup) {
                 content: 'Informe um domínio!'
             });
         } else {
-
+            $http
+                .post('http://cryptic-peak-3065.herokuapp.com', $scope.dataDomain)
+                .success(function(response) {
+                    console.log(response);
+                    if (response.message != "" && response.message != null) {
+                        $scope.fulldomain = response.data.fqdn;
+                        $scope.message = response.message;
+                        $scope.showResult = true;
+                    } else {
+                        $ionicPopup.alert({
+                            title: 'Aviso',
+                            content: 'Erro ao pesquisar domínio, tente novamente mais tarde!'
+                        });
+                    }
+                });
         }
     }
 });
 
-checkdomain.controller('AboutCtrl', function($scope) {
-});
+checkdomain.controller('AboutCtrl', function($scope) {});
