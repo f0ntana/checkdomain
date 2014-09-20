da29fc03491386deae729287be5063683101cdc6
Raphael Azeredo <rgazeredo@gmail.com>
2014-09-19 23:07:22 -0300

    Layout page home

--- js/app.js
+++ js/app.js
@@ -43,36 +43,36 @@ checkdomain.config(function($stateProvider, $urlRouterProvider) {
       views: {
         'tab-home': {
           templateUrl: 'templates/tab-home.html',
-          controller: 'DashCtrl'
+          controller: 'HomeCtrl'
         }
       }
     })
 
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
-    })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+    // .state('tab.friends', {
+    //   url: '/friends',
+    //   views: {
+    //     'tab-friends': {
+    //       templateUrl: 'templates/tab-friends.html',
+    //       controller: 'FriendsCtrl'
+    //     }
+    //   }
+    // })
+    // .state('tab.friend-detail', {
+    //   url: '/friend/:friendId',
+    //   views: {
+    //     'tab-friends': {
+    //       templateUrl: 'templates/friend-detail.html',
+    //       controller: 'FriendDetailCtrl'
+    //     }
+    //   }
+    // })
 
     .state('tab.about', {
       url: '/about',
       views: {
         'tab-about': {
           templateUrl: 'templates/tab-about.html',
-          controller: 'AccountCtrl'
+          controller: 'AboutCtrl'
         }
       }
     });
--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,23 @@
 var checkdomain = angular.module('checkdomain.controllers', [])
 
-checkdomain.controller('DashCtrl', function($scope) {
+checkdomain.controller('HomeCtrl', function($scope, $ionicPopup) {
+    $scope.dataDomain = {
+        domain: "",
+        extension: "com.br"
+    };
+    $scope.showResult = false;
+
+    $scope.searchDomain = function() {
+        if ($scope.dataDomain.domain == "") {
+            $ionicPopup.alert({
+                title: 'Aviso',
+                content: 'Informe um domínio!'
+            });
+        } else {
+
+        }
+    }
 });
 
-checkdomain.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-});
-
-checkdomain.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-});
-
-checkdomain.controller('AccountCtrl', function($scope) {
+checkdomain.controller('AboutCtrl', function($scope) {
 });
